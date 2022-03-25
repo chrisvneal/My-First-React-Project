76302eaa083a9bc285d42bd19ebd188d4c60824a
Chris Neal <chrisvneal@gmail.com>
2022-03-25 06:37:58 -1000

    Test App UI w/ new interface

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from "react";
-import "./App.scss";
+import List from "./List";
 
 class App extends React.Component {
+  state = {
+    parents: [
+      {
+        name: "Michele",
+        id: 578
+      },
+      {
+        name: "Vaughn",
+        id: 789
+      }
+    ],
+    companies: [
+      {
+        name: "HHB",
+        id: 678
+      },
+      {
+        name: "BCI Security",
+        id: 991
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>My First React Project</h1>
-        </header>
+        <List list={this.state.parents} title="Parents" />
+        <List list={this.state.companies} title="Companies" />
       </div>
     );
   }
